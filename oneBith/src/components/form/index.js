d44519b6800a2501aa52b7f064d0b1b64ba0e03b
IuryPrates <prates700@gmail.com>
2022-11-14 17:38:19 -0400

    feat: add action for click button

--- oneBith/src/components/form/index.js
+++ oneBith/src/components/form/index.js
@@ -6,7 +6,7 @@ export default function Form(){
 
     const [height, setHeight] = useState(null)
     const [weight, setWeight] = useState(null)
-    const [messageImc, setMessageImc] = useState('Preencha a altura e o peso'); 
+    const [messageImc, setMessageImc] = useState('Preencha a altura e o peso..'); 
     const [imc, setImc] = useState(null)
     const [textButton, setTextButton] = useState('Calcular')
 
@@ -33,17 +33,30 @@ export default function Form(){
             <View>
                 <Text>Altura</Text>
                 <TextInput
-                placeholder='Ex: 1.72'
-                keyboardType='numeric'/>
+                onChangeText={setHeight}
+                value={height}
+                placeholder='Ex: 1.72'//placeholder preenche a caixa de texto antes do usuário interagir
+                keyboardType='numeric'//chama o teclado para o usuário
+                />
+
                 <Text>Peso</Text>
                 <TextInput
+                onChangeText={setWeight}
+                value={weight}
                 placeholder='Ex: 60.6'
-                keyboardType='numeric'/>
+                keyboardType='numeric'
+                />
             </View>
             <View>
-                <Button title='Calcular IMC'/>
+                <Button 
+                onPress={()=> validationImc()} //chamada da função de calcular quando pressionar o botão
+                title='Calcular IMC'//descrição do botão
+                />
             </View>
-            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
+            <ResultImc 
+            messageResultImc={messageImc} //mensagem setada
+            resultImc={imc} //resultado calculado
+            />
         </View>
     );
 }
