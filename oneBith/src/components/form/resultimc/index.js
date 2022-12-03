8ae03461205dbe472841da819aea2bc8f8259c7e
IuryPrates <prates700@gmail.com>
2022-12-03 10:26:53 -0400

    feat: add share button and stilize

--- oneBith/src/components/form/resultimc/index.js
+++ oneBith/src/components/form/resultimc/index.js
@@ -1,12 +1,33 @@
 import React from "react"
-import { View, Text } from 'react-native'
+import { 
+    View,
+    Text,
+    TouchableOpacity,
+    Share 
+} from 'react-native'
 import styles from "./style"
 
 export default function ResultImc(props){
-    return(
-        <View
-        style={styles.resultImc}
-        >
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message:"Meu imc é: " + props.resultImc,
+        })
+    }
+
+    return( 
+        <View style={styles.resultImc}> 
+            <View style={styles.boxShareButton}>
+                {props.resultImc != null ?
+                //botão de compartilhar só será exibido se tiver setado 
+                //um valor de resultado do cálculo.
+                <TouchableOpacity style={styles.shared}>
+                    <Text style={styles.sharedText}>Share</Text>
+                </TouchableOpacity>
+                : //caso contrário exibe uma View vazia
+                <View/>
+                }
+            </View>
             <Text style={styles.informationImc}>{props.messageResultImc}</Text>
             <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
--- oneBith/src/components/form/resultimc/style.js
+++ oneBith/src/components/form/resultimc/style.js
@@ -18,7 +18,23 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#ff0043',
         fontWeight: 'bold',
-    }
+    },
+    boxSharedButton:{
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    shared:{
+        backgroundColor: "#4EE70B",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5,
+    },
+    sharedText:{
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
+    },
 })
 
 export default styles;
